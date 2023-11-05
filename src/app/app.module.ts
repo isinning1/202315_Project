e80198bb09227d27063874c95daa74c46cca0153
Irina Sinning <i.sinning@uniandes.edu.co>
2023-11-05 07:08:15 -0500

    Ajuste al archivo app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { PlantaModule } from './planta/planta.module';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantaModule } from './planta/planta.module';
 
 @NgModule({
   declarations: [
